8e5f4adaa4ea08c852e11f40fbd867c69d1ed050
Andrew van Duivenbode <andrew@creativetransmissions.com>
2023-07-03 19:36:14 +0100

    add basics

--- src/App.svelte
+++ src/App.svelte
@@ -2,52 +2,19 @@
 	import { onMount } from 'svelte';
 	import { createScene } from "./scene";
 
-	import Deso from 'deso-protocol';
-	const deso = new Deso();
-
 	let el;
 	let src = '';
-	let nftPostHashHex = '';
+	let ethAddress = '';
 	let previewMediaUrl = '';
 	const urlParams = new URLSearchParams(window.location.search);
-  	nftPostHashHex = urlParams.get('nftPostHashHex');
-	const request = {
-		"PostHashHex": nftPostHashHex
-	};
-
-	deso.posts.getSinglePost(request).then((response)=>{
-		if(response.PostFound.ImageURLs[0]){
-			previewMediaUrl = response.PostFound.ImageURLs[0];
-		}
-	});
-
+	ethAddress = urlParams.get('ethAddress');
+	
 	onMount((el) => {
-
-		console.log('nftPostHashHex: ',nftPostHashHex);
-		createScene(el, nftPostHashHex );
+		createScene(el, ethAddress );
 	});
 
 </script>
-<div bind:this={el} id="nft-ctr" class="nft-ctr" data-nft="{nftPostHashHex}">
-	<div class="container" id="nft-img">
-		<img id="nft-preview-img" class="nft-preview" src="{previewMediaUrl}"/>
-	</div>
-	<div id="nft-overlay-holder" class="svelte-n8t4is" style="display:none;">
-		<div id="nft-overlay" class="nft-3d-overlay svelte-n8t4is">
-			<label for="3dsky" class="svelte-n8t4is">
-				<input type="checkbox" name="3dsky" id="3dsky" class="svelte-n8t4is" checked>
-				<br>Sky</label>
-			<select id="sky" class="svelte-n8t4is">
-				<option value="blue">space 1</option><option value="lightblue">space 2</option><option value="bluecloud">clouds 1</option><option value="browncloud">clouds 2</option><option value="yellowcloud">clouds 3</option></select>
-			<label for="3dfloor" class="svelte-n8t4is">
-				<input type="checkbox" name="3dfloor" id="3dfloor" class="svelte-n8t4is"><br>Room</label>
-				<div class="background-option svelte-n8t4is" style="background: rgb(0, 0, 0) none repeat scroll 0% 0%;"></div>
-			<select style="display:none;" id="animation-select" class="svelte-n8t4is"></select>
-			<label for="3dloop" style="display:none;" class="no-mobile svelte-n8t4is">
-				<input type="checkbox" name="3dloop" id="3dloop" class="svelte-n8t4is"><br>Loop</label>
-		</div>
-	</div>
-</div>	
+<div bind:this={el} id="canvas"></div>	
 
   
 
--- src/scene.js
+++ src/scene.js
@@ -1,191 +1,52 @@
 import * as THREE from 'three';    
-import * as D3D from '3d-nft-viewer'
-import { MeshBVH, acceleratedRaycast, MeshBVHVisualizer } from '3d-nft-viewer'
 
-export const createScene = (el, nftPostHashHex) => {
+let camera, renderer, scene;
 
-//initialize NFT viewer front end
-  let nftViewer = new D3D.D3DNFTViewer({
-    el: document.body,
-    defaultLoader: 'gltf',
-    ctrClass: 'nft-ctr', // Attribute of div containing post and any additionl html
-    nftDataAttr: 'data-nft', // Attribute of div.ctrClass containing post has hex
-    nftsRoute: 'http://localhost:3000/nfts', // Back end route to initialize NFTs
-    modelsRoute: 'http://localhost:3000/models', // Back end route to load models
-    linkText: 'View in 3D',
-    linkCtrCls: 'nft-viewer', // container for links such as view in 3d, view in vr
-    previewCtrCls: 'container', //container element in which to create the preview
-    skyboxes: true,
-    skyboxPath: 'http://localhost:3000/images/skyboxes/',
-    sceneryPath: 'http://localhost:3000/layouts/round_showroom/scene.gltf',
-    useShowroom: true,
-    scaleModelToHeight: 5,
-    scaleModelToWidth: 5,
-    scaleModelToDepth: 5
-  });
+export const createScene = (el, ethAddress) => {
 
-  /*** Load the model immediately, or call the following code on click of a button ***/
-  let params = {
-    nftPostHash:nftPostHashHex,
-    containerId: 'nft-ctr',
-    hideElOnLoad: 'nft-img'
-  }
-
-  nftViewer.loadNFT(params)
-  .then((item, model, pos)=>{
-      if(nftViewer.loadedItem){ 
-        // this confirms that the model is loaded
-        nftViewer.start3D(); // start animation loop 
-        showOverlay(); // display overlay
-        addOverlayListeners(nftViewer); // add listeners to overlay
-      }
-  });
-
-  /* load the nft specified by the nftPostHashHex **
-  let params = {
-    nftPostHash:nftPostHashHex,
-    containerId: 'nft-ctr',
-    hideElOnLoad: 'nft-img'
-  }
-
-  nftViewer.loadNFT(params)
-  .then((item, model, pos)=>{
-      if(nftViewer.loadedItem){ 
-        // this confirms that the model is loaded
-        nftViewer.start3D(); // start animation loop 
-        showOverlay(); // display overlay
-        addOverlayListeners(nftViewer); // add listeners to overlay
-      }
-  });
-*/
-
-
-};
- 
-/* Add overlay options */
-const showOverlay = () =>{
-    let overlay = document.querySelector('div#nft-overlay-holder');
-    overlay.style.display = 'inline-block';
-
-}
-const addOverlayListeners = (nftViewer) =>{
-
-  let skySelection = document.querySelector('select#sky');
-  skySelection.addEventListener('change',(e)=>{
-       nftViewer.setSkyBox(e.target.value);
-  });
-
-  let skyToggle = document.querySelector('input[name="3dsky"]');
-      skyToggle.addEventListener('change',(e)=>{
-      let checked = e.target.checked;
-      if(checked){
-        nftViewer.setSkyBox(skySelection.value);
-      } else {
-        nftViewer.removeSky();
-      };
-  });
-
-  let roomToggle = document.querySelector('input[name="3dfloor"]');
-      roomToggle.addEventListener('change',(e)=>{
-      let checked = e.target.checked;
-      if(checked){
-        console.log('addScenery');
-
-        nftViewer.addScenery();
-      } else {
-        console.log('removeScenery');
-
-        nftViewer.removeScenery();
-      };
-  });
-
-  let animations = nftViewer.loadedItem.getAnimations();
-  if(!animations){
-    hideAnimationOptions();
-
-  } else {
-    let animationSelection = document.querySelector('select#animation-select');
-
-    populateAnimations(animationSelection, animations);
-    showAnimationOptions();
-    animationSelection.addEventListener('change',(e)=>{
-      if(e.target.value>1){
-        let loopType = getLoopTypeFromUI();
-        let animIndex = parseInt(e.target.value)-1;
-        console.log('startAnimation: '+animIndex);
-        nftViewer.loadedItem.startAnimation(animIndex, THREE.LoopRepeat);
-      } else {
-        nftViewer.loadedItem.stopAnimation();
-      }
-    });
-  }
- 
-}
-
-const populateAnimations = (selectEl, animations) =>{
-
-  let options = selectEl.getElementsByTagName('option');
   
-  for (var i=options.length; i--;) {
-      selectEl.removeChild(options[i]);
-  };
+  initScene()
+  fetchNFTs().then()
 
-  addOption(selectEl,-1, 'Play/Pause');
-  for (var i = 0; i<animations.length; i++){
-    let optionText = animations[i].name + ': '+animations[i].duration;
-    addOption(selectEl,i, optionText);
-  };
 
 };
 
-const addOption =  (selectEl,i, optionText) =>{
-    var opt = document.createElement('option');
-    opt.value = i+1;
-    opt.innerHTML = optionText;
-    selectEl.appendChild(opt);
+const initScene = ()=>{
+  camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+
+
+  camera.position.z = 50;
+  camera.position.y = 15;  
+  camera.lookAt(new THREE.Vector3(0,0,0));
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  scene = new THREE.Scene();  
+
+  scene.add(camera);
+  document.body.appendChild(renderer.domElement);  
+
 }
 
-const showAnimationOptions = () =>{
-  let animationSelection = document.querySelector('select#animation-select');
-      animationSelection.style.display = 'inline-block';
-
-  let loopSelection = document.querySelector('label[for="3dloop"]');
-      loopSelection.style.display = 'inline-block';
+const fetchNFTs = async ()=>{
+  try {
+    await Moralis.start({
+      apiKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub25jZSI6IjJjNmNmMjk3LTMwNzgtNDkwZi1iZjFmLTM3ZGZmZDk5NTllNyIsIm9yZ0lkIjoiMzQ2NTYyIiwidXNlcklkIjoiMzU2MjQ2IiwidHlwZUlkIjoiYzYxNzA2NjMtODU4Yi00MmQ2LWE4NjUtMTIxNTM1ZTQ2OTRmIiwidHlwZSI6IlBST0pFQ1QiLCJpYXQiOjE2ODg0MDQ3NzcsImV4cCI6NDg0NDE2NDc3N30.hhoVUYNe3cRiaqq9JMotSzMrDxTp_dg7SyC-vuu1vNU"
+    });
+  
+    const response = await Moralis.EvmApi.nft.getWalletNFTs({
+      "chain": "0x1",
+      "format": "decimal",
+      "mediaItems": false,
+      "address": "0xd8da6bf26964af9d7eed9e03e53415d37aa96045"
+    });
+  
+    console.log(response.raw);
+  } catch (e) {
+    console.error(e);
+  }  
 }
-const hideAnimationOptions =() =>{
-    let animationSelection = document.querySelector('select#animation-select');
-      animationSelection.style.display = 'none';
-
-  let loopSelection = document.querySelector('label[for="3dloop"]');
-      loopSelection.style.display = 'none';
-}
-
-const getLoopTypeFromUI = () =>{
-  let loopToggle = document.querySelector('input[name="3dloop"]');
-  if(loopToggle.checked){
-    return THREE.LoopRepeat;
-  } else {
-    return THREE.LoopOnce;
-  }
-}
-
-
-/*** Example 1 - Load a model which is not an NFT ***
-  let params = {
-    modelUrl: 'http://localhost:3000/models/19f83fd2bbc0c5ef7ac31cccc067c765447016e60b68e4f0ddcba1e278c3fd89/gltf/normal/NanoWorld_gltf.gltf',
-    containerId: 'nft-ctr',
-    hideElOnLoad: 'nft-img"'
-  }
-/*
-  nftViewer.loadModel(params)
-  .then((item, model, pos)=>{
-    // hide the loading message
-    let loadingMessage = document.querySelector('.nft-viewer');
-        loadingMessage.style.display = 'none';
-        nftViewer.showOverlay();
-        nftViewer.start3D();
-  });
-
-*/
-
-
